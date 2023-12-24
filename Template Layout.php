8dd2b757601e96c7b4ae981461405fa8b06f5751
alfidaulay123 <91549794+alfidaulay123@users.noreply.github.com>
2023-12-24 15:45:48 +0700

    Add files via upload

---
+++ Template Layout.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="styledashboard.css" rel="stylesheet" >
+    
+
+</head>
+<body>
+
+<div class="container-fluid">
+    <div class="row">
+
+        <!-- Sidebar -->
+        <div class="col-md-2 sidebar p-0">
+            <img src="IMG\Logo\Logo Crop.png" alt="Logo" class="d-inline-block align-text-top logo">
+            <table class="nav flex-column">
+                <!-- Sidebar Links -->
+                <tr class="nav-item">
+                    <td><a class="nav-link active option" href="Dashboard.php">Dashboard</a></td>
+                </tr>
+                <tr class="nav-item">
+                    <td><a class="nav-link active option" href="Peminjaman.php">Peminjaman</a></td>
+                </tr>
+                <tr class="nav-item">
+                    <td><a class="nav-link active option" href="Master Kendaraan.php">Master Kendaraan</a></td>
+                </tr>
+                <tr class="nav-item">
+                    <td><a class="nav-link active option" href="Master Setup.php">Master Setup</a></td>
+                </tr>
+                <tr class="nav-item">
+                    <td><a class="nav-link active option" href="Rekapitulasi.php">Rekapitulasi</a></td>
+                </tr>
+                <tr class="nav-item">
+                    <td><a class="nav-link active option" href="Laporan.php">Laporan</a></td>
+                </tr>
+            </table>
+        </div>
+        
+        <!-- Navbar -->
+        <div class="row justify-content-center">
+                <div class="col-12 col-md-6">
+                <nav class="navbar navbar-expand-lg bg-body-tertiary" style="max-width: 100%;">
+                    <div class="container-fluid">
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <form class="d-flex" role="search">
+                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                            </form>
+                            <img class="logout" style="height: 30.07px" src="IMG\Logo\logout.png"/>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+
+        <!-- Code Here -->
+        
+<!-- Bootstrap Scripts -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
